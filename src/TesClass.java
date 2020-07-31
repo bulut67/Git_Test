6a33006f1ed2f042e8bee07afe93a07b6792d1b9
bulut67 <69051369+bulut67@users.noreply.github.com>
2020-07-31 20:58:04 +0300

    Update TesClass.java

--- src/TesClass.java
+++ src/TesClass.java
@@ -1,5 +1,7 @@
 public class TesClass {
     public static void main(String[] args) {
         System.out.println("Git den merhaba");
+                System.out.println("GitHub dan merhaba");
+
     }
 }
